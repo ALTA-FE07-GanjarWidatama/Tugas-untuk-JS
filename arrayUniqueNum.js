7417b383492fd278a0f16f8a23889920f668b8e7
Ganjar Widatama <gwidatama@gmail.com>
2022-07-01 00:51:55 +0700

    tugas beres

--- arrayUniqueNum.js
+++ arrayUniqueNum.js
@@ -1,12 +1,23 @@
-let arrayUnique = ([1, 2, 3, 4, 1, 3, 5, 10, 16]);
-
-let b = [];
-
-let len = arrayUnique.length;
-
-for(let i = 0; i < len; i++){
-    if(b.indexOf(arrayUnique[i]) === -1){
-        b.push(arrayUnique[i]);
-    }else (arrayUnique)
+function arrayUnique(arrayA, arrayB) {
+  // Your Code Here
+  let result = [];
+  for (let i = 0; i < arrayA.length; i++) {
+    let double = 0;
+    for (let j = 0; j < arrayA.length; j++) {
+      if (arrayA[i] == arrayB[j]) {
+        double += 1;
+      }
+    }
+    if (double == 0) {
+      result.push(arrayA[i]);
+    }
+    double = 0;
+  }
+  return result;
 }
-console.log(b);
+
+console.log(arrayUnique([1, 2, 3, 4], [1, 3, 5, 10, 16])); // [2, 4]
+console.log(arrayUnique([10, 20, 30, 40], [5, 10, 15, 59])); // [20, 30, 40]
+console.log(arrayUnique([1, 3, 7], [1, 3, 5])); // [7]
+console.log(arrayUnique([3, 8], [2, 8])); // [3]
+console.log(arrayUnique([1, 2, 3], [3, 2, 1])); // []
